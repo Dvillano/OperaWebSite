e43a40e6ddb3e4fca27f8f57931aa588b8afd635
Dvillano <danie.villano@gmail.com>
2021-11-08 12:53:15 -0300

    Agregado TestController

--- OperaWebSite/App_Start/RouteConfig.cs
+++ OperaWebSite/App_Start/RouteConfig.cs
@@ -14,10 +14,33 @@ namespace OperaWebSite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "{controller}/{action}/{name}/{role}",
+                defaults: new { controller = "Test", action = "Login" }
+                );
+
+            routes.MapRoute(
+                name: "SearchByTitle",
+                url: "{controller}/buscarTitulo/{title}",
+                defaults: new { controller = "Test", action = "SearchByTitle" }
+                );
+
+            // Ruta default
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            //routes.MapRoute(
+            //    name: "PhotoRoute",
+            //    url: "photo/{id}",
+            //    defaults: new { controller = "Photo", action = "Details" },
+            //    constraints: new { id = "[0-9]+" }
+            //);
+
+
+
         }
     }
 }
